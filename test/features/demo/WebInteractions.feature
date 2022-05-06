6cdbb75b75eb0945e1f740798b43c4b62071dafc
baala-cg <baala.cg@gmail.com>
2022-05-06 18:32:37 +0530

    new fature update

---
+++ test/features/demo/WebInteractions.feature
@@ -0,0 +1,10 @@
+Feature: Web Interactions Feature
+
+    @demo
+    Scenario Outline: Demo Web Interactions
+        Given Web page is launched
+        When Perform web interactions
+
+        Examples:
+            | TestID   |
+            | Web_TC01 |
--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -21,3 +21,41 @@ Then(/^Url should match (.*)$/, async function (expectedUrl) {
   let url = await browser.getUrl();
   chai.expect(url).to.be.equal(expectedUrl);
 });
+/**
+ * Web Interactions
+ */
+Given(/^Web page is launched$/, async function () {
+  await browser.url("/inputs");
+  await browser.setTimeout({ pageLoad: 10000, implicit: 15000 });
+  await browser.maximizeWindow();
+  await browser.pause(5000);
+});
+
+When(/^Perform web interactions$/, async function () {
+  /**
+   * Input box
+   * 1.Type into input box
+   * 2.Clear the field and add value
+   * 3.Click and type
+   * 4.Type slowly
+   *
+   */
+
+  let ele = await $("[type=number]");
+  await ele.scrollIntoView(); //optional - to scroll to the element position
+  await ele.click(); //optional
+  let num = 5555;
+  let str = num.toString();
+  await ele.setValue(str);
+  await browser.pause(3000);
+  await ele.addValue("54321");
+
+  await ele.click(); //to focus on the element to enter chars
+
+  for (let i = 0; i < str.length; i++) {
+    let ch = await str.charAt(i)
+    await browser.pause(1000);
+    await browser.keys(ch);
+  }
+  // await browser.debug();
+});
